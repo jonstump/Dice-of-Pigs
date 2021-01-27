fe339b6dfde2a766b210cd866c59ecea1e1102f2
Jon Stump <jmstump@gmail.com>
2021-01-27 13:55:01 -0800

    clean up business logic and save in a finished state.
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,19 @@ function Player(playerName) {
   this.playerName = playerName;
   this.turnsTaken = 0;
   this.turnScore = 0;
-  this.scoreTotal = 98;
-  //this.scoreTotal = 0;
+  this.scoreTotal = 0;
 }
 
 Player.prototype.rollDie = function() {
-  let currentRoll = 3;
-  // const currentRoll = Math.floor(Math.random() * Math.floor(6)) + 1;
+  const currentRoll = Math.floor(Math.random() * Math.floor(6)) + 1;
   if (currentRoll === 1) {
     this.turnScore = 0; 
     this.turnsTaken++;
   } else {
-      this.turnScore = this.turnScore + currentRoll;
-      this.winCheck();
-  } return currentRoll;
+    this.turnScore = this.turnScore + currentRoll;
+    this.winCheck();
+  }
+  return currentRoll;
 } 
 
 Player.prototype.winCheck = function() {
@@ -25,8 +24,9 @@ Player.prototype.winCheck = function() {
   }
 }
 
-function rollDie() {
-  return Math.floor(Math.random() * Math.floor(6)) + 1;
+Player.prototype.hold = function() {
+  this.scoreTotal += this.turnScore;
+  this.turnsTaken++;
 }
 
 //User Interface Logic
